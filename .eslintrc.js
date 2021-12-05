6a938a9d47bd2e67a03f27b81dfdaeba4d555fbb
zhao-huo-long <lijiuyi1995@outlook.com>
2021-12-05 17:28:49 +0800

    first

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": { "project": ["./tsconfig.json"] },
+  "plugins": [
+    "@typescript-eslint"
+  ]
+};
---
+++ src/utils/sliceFile.ts
@@ -0,0 +1,7 @@
+
+
+const sliceFile = () => [
+
+]
+
+export default sliceFile
