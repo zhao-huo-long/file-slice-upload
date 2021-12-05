f241773ff31ba946a2d6e50d5f12f2e71b9dd923
zhao-huo-long <lijiuyi1995@outlook.com>
2021-12-05 17:53:41 +0800

    file slice

--- src/utils/sliceFile.ts
+++ src/utils/sliceFile.ts
@@ -1,7 +1,24 @@
 
+export type BuildNameFunt = (file: File, index: number) => string
 
-const sliceFile = () => [
+const buildChunkName: BuildNameFunt = (file, index) => {
+  return `${file.name}-${index}`
+}
 
-]
+/** file slice to chunks */
+const sliceFile = (file: File, chunkSize?: 0, chunkName?: BuildNameFunt) : File[] => {
+  let i = 0;
+  const chunkNums =  Math.ceil(file.size / chunkSize)
+  const chunksArray = new Array(chunkNums) as File[]
+  while(i < chunkNums){
+    const chunkBlob = file.slice(i * chunkSize, i * chunkSize + chunkSize)
+    const chunkFileName = chunkName ? chunkName(file, i) : buildChunkName(file, i)
+    chunksArray.push(new File([chunkBlob], chunkFileName, {
+      type: file.type
+    }))
+    i++
+  }
+  return chunksArray
+}
 
 export default sliceFile
